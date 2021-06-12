3518260b5f0c883c76d6aa795f62b9fe19ab1fd0
nomaxg <noahgolub2@gmail.com>
2021-06-12 19:42:19 -0400

    added basic wall jumping

--- entities/actor/actor.gd
+++ entities/actor/actor.gd
@@ -6,7 +6,8 @@ extends KinematicBody2D
 
 
 export var speed = Vector2(300.0, 700.0)
-onready var gravity = ProjectSettings.get("physics/2d/default_gravity")
+onready var default_gravity = ProjectSettings.get("physics/2d/default_gravity")
+onready var gravity = default_gravity
 
 const FLOOR_NORMAL = Vector2.UP
 
--- entities/actor/player.gd
+++ entities/actor/player.gd
@@ -5,6 +5,8 @@ extends Actor
 # var b = "text"
 
 const COYOTE_TIME = 0.1
+const WALL_IMPULSE = 3000
+const WALL_FRICTION = 0.1
 
 onready var FLOOR_DETECT_DISTANCE = $PlatformDetector.cast_to.y
 
@@ -12,11 +14,19 @@ onready var platform_detector = $PlatformDetector
 onready var animation_player = $AnimationPlayer
 onready var sprite = $SpriteContainer/Sprite
 onready var bounce_detector = $BounceDetector
+onready var left_wall_cast = $LeftWallCast
+onready var right_wall_cast = $RightWallCast
 
 var queue_jump = false
 var is_jumping = false
+var just_wall_jumped 
 var airborne: float = 0.0
 
+enum Wall {
+	NONE = 0
+	RIGHT = 1,
+	LEFT = -1,
+}
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -25,10 +35,23 @@ func _ready():
 #func _process(delta):
 #	pass
 
+func _wall_touched():
+	if left_wall_cast.is_colliding():
+		return Wall.LEFT
+	elif right_wall_cast.is_colliding(): 
+		return Wall.RIGHT
+	else:
+		return Wall.NONE
 
+func _is_touching_wall():
+	var wall_touched = _wall_touched()
+	return wall_touched == Wall.RIGHT || wall_touched == Wall.LEFT
+	
 func _physics_process(_delta):
 	var input_direction: float = get_direction()
-	
+	var wall_jumped_off = Wall.NONE
+	gravity = default_gravity
+
 	if is_on_floor():
 		airborne = 0
 		is_jumping = false
@@ -40,17 +63,26 @@ func _physics_process(_delta):
 	
 	if can_jump():
 		if Input.is_action_pressed("jump"):
+			if _wall_touched() != Wall.NONE && Input.is_action_just_pressed("jump):
+				wall_jumped_off = _wall_touched()
 			_velocity.y = -speed.y
+			
 			is_jumping = true
 			queue_jump = false
 			Input.action_press("jump")
 
 	var is_jump_interrupted = is_jumping and Input.is_action_just_released("jump") and _velocity.y < 0.0
-	_velocity = calculate_move_velocity(_velocity, input_direction, speed, is_jump_interrupted)
-
+	_velocity = calculate_move_velocity(_velocity, input_direction, speed, is_jump_interrupted, wall_jumped_off)
+	print(_velocity)
+	
 	# Slightly raise the slime when jumping so that it can jump on rising platform
 	if _velocity.y < 0:
 		position.y -= 0.5
+		
+	# Add friction when sliding down a wall
+	if _is_touching_wall() && _velocity.y > 0:
+		print("APPLY FRICTION")
+		gravity *= WALL_FRICTION
 
 	var snap_vector = Vector2.ZERO
 	if not (Input.is_action_just_pressed("jump") or queue_jump):
@@ -73,16 +105,20 @@ func _physics_process(_delta):
 
 
 func can_jump():
-	return is_on_floor() or (airborne < COYOTE_TIME and not is_jumping)
+	return is_on_floor() or (airborne < COYOTE_TIME and not is_jumping) or _is_touching_wall()
 
 func calculate_move_velocity(
 		linear_velocity: Vector2,
 		direction,
 		speed,
-		is_jump_interrupted
+		is_jump_interrupted,
+		wall_jumped_off
 	) -> Vector2:
 	var velocity = linear_velocity
-	velocity.x = speed.x * direction
+	if wall_jumped_off != Wall.NONE:
+		velocity.x = wall_jumped_off * -WALL_IMPULSE
+	else: 
+		velocity.x = speed.x * direction
 	if is_jump_interrupted:
 		# Decrease the Y velocity by multiplying it, but don't set it to 0
 		# as to not be too abrupt.

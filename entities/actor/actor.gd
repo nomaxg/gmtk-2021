d2a95b6fcb701e4f52b11c6917425265cd22a2ac
nomaxg <noahgolub2@gmail.com>
2021-06-12 15:45:05 -0400

    added back platform scene

--- entities/actor/actor.gd
+++ entities/actor/actor.gd
@@ -8,7 +8,6 @@ extends KinematicBody2D
 export var speed = Vector2(300.0, 700.0)
 onready var gravity = ProjectSettings.get("physics/2d/default_gravity")
 
-
 const FLOOR_NORMAL = Vector2.UP
 
 var _velocity = Vector2.ZERO

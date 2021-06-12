248d8914fec100689fa8e2cfde46306a2b2bec5b
nomaxg <noahgolub2@gmail.com>
2021-06-12 14:26:02 -0400

    basic moving platforms

--- entities/actor/actor.gd
+++ entities/actor/actor.gd
@@ -8,6 +8,7 @@ extends KinematicBody2D
 export var speed = Vector2(300.0, 700.0)
 onready var gravity = ProjectSettings.get("physics/2d/default_gravity")
 
+
 const FLOOR_NORMAL = Vector2.UP
 
 var _velocity = Vector2.ZERO
